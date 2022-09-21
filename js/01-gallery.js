e3c775d768b462ec4980c34323b1ac4e77ce8f1c
Mykhail92 <movemens1992@gmail.com>
2022-09-21 20:27:50 +0300

    complete task 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,44 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
-console.log(galleryItems);
+const refsGallery = document.querySelector(".gallery");
+
+function createItem(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
+<a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`;
+    })
+    .join("");
+}
+// console.log(createItem(galleryItems));
+const addCreateItem = createItem(galleryItems);
+refsGallery.insertAdjacentHTML("beforeend", addCreateItem);
+
+refsGallery.addEventListener("click", onClick);
+// refsGallery.addEventListener("keydown", onEscape);
+
+function onClick(event) {
+  event.preventDefault();
+  const dataSource = event.target.dataset.source;
+  if (!dataSource) {
+    return;
+  }
+  const instance = basicLightbox.create(`
+  <img src="${dataSource}" width="800" height="600">
+`);
+  instance.show();
+
+  refsGallery.addEventListener("keydown", (event) => {
+    if (event.key === "Escape") {
+      instance.close();
+    }
+  });
+}
