956f9ab681414477f31ad1713a206f88cf114a52
Mykhail92 <movemens1992@gmail.com>
2022-09-22 17:40:12 +0300

    complete task 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,20 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
-console.log(galleryItems);
+const refsGallery = document.querySelector(".gallery");
+
+function createItem(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" title= "${description}" alt="${description}" />
+</a>`;
+    })
+    .join("");
+}
+const addCreateItem = createItem(galleryItems);
+refsGallery.insertAdjacentHTML("beforeend", addCreateItem);
+
+let lightbox = new SimpleLightbox(".gallery a", {
+  captionDelay: 250,
+});
+console.dir(lightbox);
